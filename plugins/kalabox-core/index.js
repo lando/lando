92bebc9f422b7207427a43e7e18d9891e8980a46
Mike Pirog <mike@kalamuna.com>
2017-01-11 09:47:19 -0800

    Clean up plugin system

---
+++ plugins/kalabox-core/index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+module.exports = function(lando) {
+  // Load CLI tasks for this plugin.
+  //require('./lib/tasks.js')(kbox);
+  // Load core events for this plugin.
+  //require('./lib/events.js')(kbox);
+  lando.log.silly('hello');
+};
