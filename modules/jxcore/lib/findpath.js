6ade85f9fbb24502fcd9fc2d2601e143a9318811
Mike Pirog <mike@kalamuna.com>
2017-02-11 15:06:04 -0800

    #6: Add JXCORE module, will remove this once we start using electron

---
+++ modules/jxcore/lib/findpath.js
@@ -0,0 +1,13 @@
+var fs = require('fs');
+var path = require('path');
+var bindir = path.resolve(__dirname, '..', 'bin');
+
+module.exports = function() {
+  var bin = bindir;
+  if (process.platform === 'win32') {
+    bin = path.join(bin, 'jx.exe');
+  } else {
+    bin = path.join(bin, 'jx');
+  }
+  return bin;
+}
