4fa156f574fa5924599157d1c18371ca345fd4d2
Mike Pirog <mike@kalamuna.com>
2017-02-11 15:55:11 -0800

    #6: Clean up since JXCORE is not workable anymore

--- tasks/common.js
+++ tasks/common.js
@@ -5,10 +5,19 @@
  * This file/module contains common things needed by many tasks.
  */
 
+// Grab needed dataz
+var pkg = require('./../package.json');
+
 // System info
 var system = {
   platform: (process.platform !== 'darwin') ? process.platform : 'osx'
 };
+
+// Lando info
+var version = pkg.version;
+var pkgType = [system.platform, 'x64', 'v' + version].join('-');
+var pkgExt = (system.platform === 'win32') ? '.exe' : '';
+var pkgSuffix = pkgType + pkgExt;
 
 // All js files
 var jsFiles = [
@@ -22,10 +31,26 @@ var jsFiles = [
   'tests/**/*.js'
 ];
 
+// Build assets
+var buildFiles = [
+  'bin/lando.*',
+  'lib/**',
+  'plugins/**',
+  '*.json',
+  'config.yml'
+];
+
 // Return our objects
 module.exports = {
   system: system,
+  lando: {
+    version: version,
+    pkgType: pkgType,
+    pkgExt: pkgExt,
+    pkgSuffix: pkgSuffix
+  },
   files: {
-    js: jsFiles
+    js: jsFiles,
+    build: cliBuildFiles
   }
 };
