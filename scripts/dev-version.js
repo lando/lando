2f0577a182e4be1eebaa3bffb28a8fe9cf920e2c
Mike Pirog <mike@kalamuna.com>
2017-02-12 20:43:45 -0800

    #5: Deploy latest build to s3

--- scripts/dev-version.js
+++ scripts/dev-version.js
@@ -10,7 +10,7 @@ var _ = require('lodash');
 var bump = require('./../tasks/util.js')();
 var fs = require('fs-extra');
 var path = require('path');
-var Promise = require('./../lib/promise.js');
+var Promise = require('./../lib/promise');
 var shell = require('shelljs');
 
 // Get the location of the files we need to edit
@@ -20,7 +20,7 @@ var files = bump.bump.options.files;
 return new Promise(function(resolve, reject) {
   shell.exec(['git describe'], {silent: true}, function(code, stdout, stderr) {
     if (code !== 0) {
-      reject(new VError('code: ' + code + 'err:' + stderr));
+      reject(new Error('code: ' + code + 'err:' + stderr));
     }
     else {
       resolve(stdout);
---
+++ tasks/util.js
@@ -0,0 +1,34 @@
+'use strict';
+
+/**
+ * This file/module contains helpful util tasks.
+ */
+
+module.exports = function() {
+
+  /*
+   * Increments the version number, etc.
+   */
+  return {
+    bump: {
+      options: {
+        files: ['package.json'],
+        updateConfigs: [],
+        commit: true,
+        commitMessage: 'Release v%VERSION%',
+        commitFiles: ['package.json'],
+        createTag: true,
+        tagName: 'v%VERSION%',
+        tagMessage: 'Version %VERSION%',
+        push: true,
+        pushTo: 'origin',
+        gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d',
+        globalReplace: false,
+        prereleaseName: 'alpha',
+        metadata: '',
+        regExp: false
+      }
+    }
+  };
+
+};
