3362bc1736492f1b6a77422075dd57a4cd180b6d
Mike Pirog <mike@kalamuna.com>
2017-02-13 16:38:43 -0800

    #5: Setup basic mochachai unit testing framework

---
+++ test/unit/cache.spec.js
@@ -0,0 +1,37 @@
+/**
+ * Basic test to just confirm our framework works
+ * @file test.js
+ */
+
+/*jshint expr:true */
+
+'use strict';
+
+// Setup chai.
+var chai = require('chai');
+var expect = chai.expect;
+chai.should();
+
+// Get caching module to test
+var cache = require('./../../lib/cache');
+
+// This is the file we are testing
+describe('cache', function() {
+
+  // This is the method we are testing
+  describe('#get', function() {
+
+    // Retrieving a stale key should result in nothing
+    it('returns undefined when grabbing an unset key', function() {
+
+      // Get the result of a key that has not been set
+      var result = cache.get('BOGUSKEY-I-LOVE-NICK3LBACK-4-LYF');
+
+      // What were you expecting?
+      expect(result).to.be.undefined;
+
+    });
+
+  });
+
+});
