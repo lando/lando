8c8b31f604ebe70970345cbb50b7d409857ea429
Mike Pirog <mike@kalamuna.com>
2017-02-21 01:17:55 -0500

    #8: Basic nginx and apache php variants

--- plugins/lando-services/php/5.3-apache/README.md
+++ plugins/lando-services/php/5.3-apache/README.md
@@ -8,10 +8,7 @@ A decent cross purpose apache based php 5.3 appserver.
 #
 # docker build -t kalabox/php:5.3-apache .
 
-FROM eugeneware/php-5.3
-
-# Set a harmless default for our extras
-ENV LANDO_EXTRAS true
+FROM eugeneware/php-5.3:master
 
 # Copy our helpers
 COPY docker-php-ext-* /usr/local/bin/
@@ -66,7 +63,4 @@ RUN apt-get update && apt-get install -y \
   && apt-get -y autoclean \
   && apt-get -y autoremove \
   && rm -rf /var/lib/apt/lists/* && rm -rf && rm -rf /var/lib/cache/* && rm -rf /var/lib/log/* && rm -rf /tmp/*
-
-# Run any additional commands that are passed in
-RUN "$LANDO_EXTRAS"
 ```
--- plugins/lando-services/php/php.js
+++ plugins/lando-services/php/php.js
@@ -9,7 +9,8 @@
 module.exports = function(lando) {
 
   // Modules
-  var path = require('path');
+  var _ = require('lodash');
+  var addConfig = lando.services.addConfig;
 
   /**
    * Supported versions for php
@@ -23,48 +24,132 @@ module.exports = function(lando) {
     'custom'
   ];
 
+  /*
+   * Parse our config
+   */
+  var parseConfig = function(config) {
+
+    // Get the version and type
+    var version = config.version || '7.0';
+    var type = config.via.split(':')[0] || 'nginx';
+
+    // Define type specific config things
+    var typeConfig = {
+      nginx: {
+        type: type,
+        command: ['php-fpm'],
+        image: [version, 'fpm'].join('-'),
+        serverConf: '/etc/nginx/conf.d/default.conf'
+      },
+      apache: {
+        type: type,
+        command: ['apache2-foreground'],
+        image: [version, 'apache'].join('-')
+      }
+    };
+
+    // Add the docker php entrypoint if we are on a supported php version
+    if (version.split('.').join('') > 55) {
+      typeConfig[type].command.unshift('docker-php-entrypoint');
+    }
+
+    // Return type specific config
+    return _.merge(config, typeConfig[type]);
+
+  };
+
+  /*
+   * Build php
+   */
+  var php = function(config) {
+    return {
+      image: 'kalabox/php:' + config.image,
+      environment: {
+        TERM: 'xterm'
+      },
+      volumes: [],
+      command: config.command.join(' '),
+      'volumes_from': ['data']
+    };
+  };
+
+  /*
+   * Build nginx
+   */
+  var nginx = function(config) {
+
+    // Build base service
+    var nginx = {
+      image: config.via || 'nginx',
+      links: [config.name + '-php:fpm'],
+      volumes: [],
+      'volumes_from': ['data'],
+      command: 'nginx -g "daemon off;"'
+    };
+
+    // Add relevant config files
+    var defaultConfig = ['php', 'default.conf'];
+    var serverFile = config.serverConf;
+    nginx.volumes.push(addConfig(defaultConfig, serverFile));
+
+    // Return the relevant service
+    return nginx;
+
+  };
+
+  /*
+   * Build a starting point for our service
+   * lets delegate this since php is complicated
+   */
+  var buildAppserver = function(config) {
+
+    // Get name and type
+    var name = config.name;
+    var type = config.type;
+
+    // Start a services collector
+    var services = {};
+
+    // Build the correct "appserver"
+    services[name] = (type === 'nginx') ? nginx(config) : php(config);
+
+    // Start up the normal ports
+    services[name].ports = ['80'];
+
+    // Add fpm backend if we are doing nginx
+    if (type === 'nginx') {
+      var backend = [name, 'php'].join('-');
+      services[backend] = php(config);
+    }
+
+    // Return things
+    return services;
+
+  };
+
   /**
    * Build out php
    */
   var builder = function(name, config) {
 
-    // Get some config
-    var version = config.version || '7.0';
-    var via = config.via || 'nginx';
+    // Parse our config
+    config.name = name;
+    config = parseConfig(config);
 
-    // Start a services collector
-    var services = {};
+    // Get basic services things
+    var services = buildAppserver(config);
 
     // Define config mappings
     var configFiles = {
-      webroot: '/var/www/html'
+      webroot: '/var/www/html',
+      server: config.serverConf
     };
 
-    // Build file
-    var dockerDir = path.join([version, via].join('-'));
-    var buildFile = path.join(lando.config.engineConfigDir, 'php', dockerDir);
-
-    // Default php service
-    var php = {
-      //image: 'php:',
-      build: buildFile,
-      ports: ['80'],
-      environment: {
-        TERM: 'xterm'
-      },
-      command: 'docker-php-entrypoint apache2-foreground',
-      volumes: [],
-      'volumes_from': ['data']
-    };
-
-    // Add the data container
+    // Add the datacontainer
     services.data = {
       image: 'busybox',
       volumes: [configFiles.webroot]
     };
-
-    // Put it all together
-    services[name] = php;
 
     // Return our service
     return services;
