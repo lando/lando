2725698d21cdcd81c7b97c99861e00cdf403b2e2
Mike Pirog <mike@kalamuna.com>
2017-06-26 09:54:27 -0700

    #110: Autocreate .lando.yml file

--- plugins/lando-core/lib/tasks/init.js
+++ plugins/lando-core/lib/tasks/init.js
@@ -11,11 +11,14 @@ module.exports = function(lando) {
   // Modules
   var _  = lando.node._;
   var chalk = lando.node.chalk;
+  var fs = lando.node.fs;
+  var path = require('path');
 
   // Create the default set of options/questions
   var defaultOpts = {
     recipe: {
-      describe: 'Specify the recipe with which we init',
+      describe: 'The recipe to use',
+      choices: lando.recipes.get(),
       alias: ['r'],
       string: true,
       interactive: {
@@ -24,7 +27,8 @@ module.exports = function(lando) {
         default: 'custom',
         choices: _.map(lando.recipes.get(), function(recipe) {
           return {name: recipe, value: recipe};
-        })
+        }),
+        weight: 70
       }
     },
     destination: {
@@ -35,7 +39,7 @@ module.exports = function(lando) {
         type: 'input',
         message: 'Where do you want to create this app?',
         default: process.cwd(),
-        weight: 98
+        weight: 80
       }
     },
     webroot: {
@@ -45,7 +49,7 @@ module.exports = function(lando) {
         type: 'input',
         message: 'Where is your webroot relative to the init destination?',
         default: '.',
-        weight: 99
+        weight: 90
       }
     },
     yes: {
@@ -58,17 +62,48 @@ module.exports = function(lando) {
         message: 'Initialize?',
         weight: 100
       }
+    },
+    overwrite: {
+      interactive: {
+        type: 'confirm',
+        message: 'Are you sure you want to overwrite existing .lando.yml?',
+        weight: 110,
+        when: function(answers) {
+
+          // Get things to check
+          var lyaml = path.join(answers.destination, '.lando.yml');
+          var yes = lando.tasks.argv().yes;
+          var hasYaml = fs.existsSync(lyaml);
+
+          // Determine whether to show
+          return hasYaml && !yes;
+
+        }
+      }
     }
   };
 
+  // @todo: Merge in additinal questions from any sources
+
   // The task object
   return {
+    // @todo: add in [github|pantheon|etc] as opt arg
     command: 'init <appname>',
-    describe: 'Initializes an app from source at target',
+    describe: 'Initializes a lando app.',
     options: defaultOpts,
     run: function(options) {
-      console.log(options);
-      console.log(chalk.red('OH YEAH!'));
+
+      // If we decline to overwrite then we are done
+      if (options.overwrite === false) {
+        console.log(chalk.yellow('Init cancelled!'));
+        return;
+      }
+
+      // Run any build tasks defined by our source selection
+
+      // Do the normal lando create jam
+      return lando.init.yamlme(options);
+
     }
   };
 
