7a7b66c9d6f3039e9b39791b4f3343f6bc06c3a4
Mike Pirog <mike@thinktandem.io>
2017-08-09 19:16:52 -0700

    #259: Fixed errors when using app and special containers at the same time

--- plugins/lando-proxy/lib/proxy.js
+++ plugins/lando-proxy/lib/proxy.js
@@ -17,6 +17,7 @@ module.exports = function(lando) {
   // Fixed location of our proxy service compose file
   var proxyDir = path.join(lando.config.userConfRoot, 'proxy');
   var proxyFile = path.join(proxyDir, 'proxy' + '.yml');
+  var projectName = 'landoproxy0ywebgwge';
 
   /*
    * Helper to extract ports from inspect data
@@ -36,7 +37,7 @@ module.exports = function(lando) {
     // Return engine object
     return {
       compose: [file],
-      project: 'lando',
+      project: projectName,
       opts: {
         services: ['proxy']
       }
@@ -205,7 +206,8 @@ module.exports = function(lando) {
         entrypoint: '/lando-entrypoint.sh',
         command: cmd,
         labels: {
-          'io.lando.container': 'TRUE'
+          'io.lando.container': 'TRUE',
+          'io.lando.service-container': 'TRUE'
         },
         environment: {
           LANDO_SERVICE_TYPE: 'proxy'
@@ -627,7 +629,7 @@ module.exports = function(lando) {
           networks: {
             'lando_proxyedge': {
               external: {
-                name: 'lando_edge'
+                name: projectName + '_edge'
               }
             }
           },
@@ -664,7 +666,7 @@ module.exports = function(lando) {
           // Add in relevant labels if we have hosts
           if (!_.isEmpty(hosts)) {
             var labels = _.get(app.services[service.name], 'labels', {});
-            labels['traefik.docker.network'] = 'lando_edge';
+            labels['traefik.docker.network'] = projectName + '_edge';
             labels['traefik.frontend.rule'] = 'Host:' + hosts.join(',');
             labels['traefik.port'] = _.toString(port);
 
