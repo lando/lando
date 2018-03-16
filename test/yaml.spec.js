f477eabd702fb3ee4dde05cd610d818b8840c06a
James Drawneek <jmdrawneek@googlemail.com>
2018-03-16 17:39:40 +0000

    Refactor more robust path resolution. Added temporary config create and removal.

--- test/yaml.spec.js
+++ test/yaml.spec.js
@@ -4,35 +4,67 @@
  */
 
 'use strict';
+const path = require('path');
+const fs = require('fs');
 
 // Setup chai.
-var chai = require('chai');
-var expect = chai.expect;
+const chai = require('chai');
+const expect = chai.expect;
 chai.should();
 
-var path = require('path');
-var yamlTools = require(path.resolve(__dirname, '../lib/yaml.js'))();
+// Load the yaml tools we'll be using to load the yaml but test functionality.
+const yamlTools = require(path.resolve(__dirname, '../lib/yaml.js'))();
+
+// Set where we're writing the test lando file to.
+const testLandoPath = path.resolve(__dirname, './.lando.yml');
+
+// Create a temporary app
+const app = {
+    'name': 'lando-variable-replacement-test',
+    'custom_variables': {
+        'node_version': 'node:8.9',
+        'var2': 'variable-2'
+    },
+    'a_setting': '${var2}', // eslint-disable-line no-template-curly-in-string
+    'services': {
+        'node': {
+            'type': '${node_version}' // eslint-disable-line no-template-curly-in-string
+        },
+        'redis': {
+            'type': 'redis:4.0'
+        }
+    }
+};
+
+
+// Write out the temp Lando file...
+yamlTools.dump(testLandoPath, app);
 // Load the test .lando.yml config file.
-var testConfig = yamlTools.load(path.resolve(__dirname, './.lando.yml'));
+const testConfig = yamlTools.load(testLandoPath);
 
 // This is the file we are testing
 describe('yaml', function() {
 
     describe('Test variable replacement in .lando.yml config file', function() {
         // Fire the replace method on the test config file.
-        var replacedConfig = yamlTools.replace(testConfig);
+        const replacedConfig = yamlTools.replace(testConfig);
 
         describe('Shallow replacement', function () {
             it('should reflect the declared variable.', function () {
-                expect(replacedConfig.name).to.equal('testingtesting.dev');
+                expect(replacedConfig.a_setting).to.equal('variable-2');
             });
         });
 
         describe('Deep replacement', function () {
             it('should reflect the declared variable.', function () {
-                expect(replacedConfig.services.appserver.type).to.equal('testingtesting');
+                expect(replacedConfig.services.node.type).to.equal('node:8.9');
             });
         });
 
     });
+});
+
+fs.unlink(testLandoPath, (err) => {
+    if (err) throw err;
+console.log('path/file.txt was deleted');
 });
