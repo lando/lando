239132739370039f50424b659b833050d15e0862
Mike Pirog <mike@thinktandem.io>
2018-03-30 19:03:07 -0700

    Update Docker for Mac version in installer

--- scripts/build-osx.sh
+++ scripts/build-osx.sh
@@ -6,7 +6,7 @@ set -e
 
 # Vars
 LANDO_VERSION=$(node -pe 'JSON.parse(process.argv[1]).version' "$(cat package.json)")
-DOCKER_VERSION="17.09.0-ce-mac35"
+DOCKER_VERSION="18.03.0-ce-mac60"
 TEAM_ID="FY8GAUX282"
 PKG_SIGN=false
 DMG_SIGN=false
