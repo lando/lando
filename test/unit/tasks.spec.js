77090e3906220e8d9001c07b3b70a859bd32cb65
Dustin LeBlanc <dustin.m.leblanc@gmail.com>
2018-04-02 13:34:24 -0400

    Validate minimum task object keys

--- test/unit/tasks.spec.js
+++ test/unit/tasks.spec.js
@@ -1,38 +1,47 @@
 /**
-* Tests for tasks module.
-* @file tasks.spec.js
-*/
+ * Tests for tasks module.
+ * @file tasks.spec.js
+ */
 
 'use strict';
 
 const chai = require('chai');
 chai.should();
+const expect = chai.expect;
 
 const tasks = require('./../../lib/tasks');
+
+const elScorcho = {
+  command: 'elscorcho [appname]',
+  describe: 'Don red hair and say that [appname] shreds the cello',
+  options: {
+    jello: {
+      describe: 'Makes [appname] jello, baby',
+      alias: ['j'],
+      default: false,
+      boolean: true,
+      interactive: {
+        type: 'confirm',
+        message: 'Are you sure you are jello?'
+      }
+    }
+  },
+  run: function(options) {
+    console.log(options);
+  }
+};
 
 describe('tasks', () => {
   describe('#add', () =>  {
     it('Adds tasks to tasks collection', () => {
-      tasks.add('El Scorcho', {
-        command: 'elscorcho [appname]',
-        describe: 'Don red hair and say that [appname] shreds the cello',
-        options: {
-          jello: {
-            describe: 'Makes [appname] jello, baby',
-            alias: ['j'],
-            default: false,
-            boolean: true,
-            interactive: {
-              type: 'confirm',
-              message: 'Are you sure you are jello?'
-            }
-          }
-        },
-        run: function(options) {
-          console.log(options);
-        }
-      });
+      tasks.add('El Scorcho', elScorcho);
       tasks.tasks[0].should.have.property('name', 'El Scorcho');
+    });
+    it('Rejects tasks that lack the minimum defined keys', () => {
+      const badTask = () => {
+        tasks.add('How You Remind Me', {describe: 'Terrible'});
+      };
+      expect(badTask).to.throw();
     });
   });
 });
