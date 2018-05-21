620041ffc67e66364f42de30f0c82d5844fb1c3a
Manuele Menozzi <manuele.menozzi@gmail.com>
2018-05-21 18:55:41 +0200

    Refactors php service test to properly use chai-as-promised

--- plugins/lando-services/test/functional/cli/php.spec.js
+++ plugins/lando-services/test/functional/cli/php.spec.js
@@ -4,42 +4,38 @@ const path = require('path');
 const helperPath = path.resolve('test', 'helpers', 'functional', 'cli.js');
 const helper = require(helperPath);
 const chai = require('chai');
+const chaiAsPromised = require('chai-as-promised');
 chai.should();
+chai.use(chaiAsPromised);
+
+// Basic PHP app.
+const app = {
+    'name': 'lando-php-test',
+    'services': {
+        'php': {
+            'type': 'php:7.1'
+        }
+    },
+    // We need this because we don't automatically expose Composer without
+    // recipes.
+    'tooling': {
+        'composer': {
+            'service': 'php'
+        }
+    }
+};
 
 describe('php', () => {
-  it('installs prestissimo via Composer', function() {
-    this.timeout(50000);
-    // Basic PHP app.
-    const app = {
-      'name': 'lando-php-test',
-      'services': {
-        'php': {
-          'type': 'php:7.1'
-        }
-      },
-      // We need this because we don't automatically expose Composer without
-      // recipes.
-      'tooling': {
-        'composer': {
-          'service': 'php'
-        }
-      }
-    };
-
-    // Create a temporary app.
-    return helper.createTestApp(app)
-
-    .then(appFolder => {
-      // Start the app
-      helper.execCommand(['start'], {cwd: appFolder})
-
-      .then(res => {
-        // Check what global Composer packages are installed
-        helper.execCommand(['composer', 'global', 'show'], {cwd: appFolder})
-
-        // Hopefull Prestissimo is one of them!
-        .then(res => res.stdout.should.have.string('hirak/prestissimo'));
-      });
+    it('installs prestissimo via Composer', function () {
+        this.timeout(50000);
+        // Create a temporary app.
+        return helper.createTestApp(app).then(function (appFolder) {
+            // Start the app
+            return helper.execCommand(['start'], {cwd: appFolder}).then(function (res) {
+                // Check what global Composer packages are installed
+                return helper.execCommand(['composer', 'global', 'show'], {cwd: appFolder}).then(res => res.stdout);
+            });
+        })
+            .should.eventually.have.string('hirak/prestissimo');
     });
-  });
 });
