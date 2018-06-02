de77259e6520f447a08ec887ef56e1b3033a35ea
Mike Pirog <mike@thinktandem.io>
2018-06-02 00:03:36 -0400

    #789: Improve existing core unit tests

--- test/unit/tasks.spec.js
+++ test/unit/tasks.spec.js
@@ -6,11 +6,13 @@
 'use strict';
 
 const chai = require('chai');
-chai.should();
 const expect = chai.expect;
+chai.should();
 
+// Get tasks module to check
 const tasks = require('./../../lib/tasks');
 
+// Define a dummy task
 const elScorcho = {
   command: 'elscorcho [appname]',
   describe: 'Don red hair and say that [appname] shreds the cello',
@@ -31,17 +33,25 @@ const elScorcho = {
   }
 };
 
+// Test the tasks
 describe('tasks', () => {
+
+  // Check out that add method!
   describe('#add', () =>  {
+
+    // Add a task to the task object
     it('Adds tasks to tasks collection', () => {
       tasks.add('El Scorcho', elScorcho);
       tasks.tasks[0].should.have.property('name', 'El Scorcho');
     });
+
+    // Fail if we dont have the right things
     it('Rejects tasks that lack the minimum defined keys', () => {
       const badTask = () => {
         tasks.add('How You Remind Me', {describe: 'Terrible'});
       };
       expect(badTask).to.throw();
     });
+
   });
 });
