efa3b925b8e4eb51263b87bcd4a899271f82699c
Mike Pirog <mike@thinktandem.io>
2018-06-10 08:13:06 -0400

    #789: spec for table.js

---
+++ test/unit/table.spec.js
@@ -0,0 +1,56 @@
+/**
+ * Tests for table module.
+ * @file table.spec.js
+ */
+
+'use strict';
+
+const _ = require('lodash');
+const chai = require('chai');
+const Table = require('./../../lib/table');
+chai.should();
+
+describe('table', () => {
+  describe('#Table', () => {
+    it('should return a Table instance with correct default options', () => {
+      const table = new Table();
+      table.should.be.instanceof(Table);
+      table.options.head.should.be.empty;
+      table.options.chars.should.have.property('bottom', '');
+    });
+  });
+
+  describe('#add', () => {
+    it('should add a cyan key and value row to a table', () => {
+      const table = new Table();
+      const data = {
+        taylor: 'swift',
+        miley: 'cyrus',
+        katy: 'perry',
+      };
+      let counter = 0;
+      _.forEach(data, (value, key) => {
+        table.add(key, value);
+        table[counter][0].should.equal('\u001b[36m' + key + '\u001b[39m');
+        table[counter][1].should.equal(value);
+        counter = counter + 1;
+      });
+    });
+
+    it('should join array values with ", " by default', () => {
+      const table = new Table();
+      const data = ['crosby', 'stills', 'nash', 'young'];
+      table.add('csny', data);
+      table[0][0].should.equal('\u001b[36mcsny\u001b[39m');
+      table[0][1].should.equal('crosby, stills, nash, young');
+    });
+
+    it('should join array values with alternate opts.arrayJoiner if specified', () => {
+      const table = new Table();
+      const data = ['crosby', 'stills', 'nash', 'young'];
+      table.add('csny', data, {arrayJoiner: '='});
+      table[0][0].should.equal('\u001b[36mcsny\u001b[39m');
+      table[0][1].should.equal('crosby=stills=nash=young');
+    });
+  });
+});
