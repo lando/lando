b12bc2fc4fa0446ced4656605cd19478891ac634
Mike Pirog <mike@thinktandem.io>
2018-06-21 13:15:29 -0400

    #1036: Complete refactor of tooling plugin

--- plugins/lando-tooling/lib/utils.js
+++ plugins/lando-tooling/lib/utils.js
@@ -5,15 +5,42 @@ const _ = require('lodash');
 const path = require('path');
 
 /*
- * Helper to process args
- *
- * We assume pass through commands so let's use argv directly and strip out
- * the first three assuming they are [node, lando.js, options.name]'
- * Check to see if we have global lando opts and remove them if we do
+ * Helper to get command
  */
-exports.stripGlobalOpts = (argopts = process.argv.slice(2)) => {
-  return (_.indexOf(argopts, '--') >= 0) ? _.slice(argopts, 0, _.indexOf(argopts, '--')) : argopts;
+const getCommand = (cmd, passOpts) => {
+  // Extract the command if its an object
+  if (_.isObject(cmd)) cmd = cmd[_.first(_.keys(cmd))];
+  // Build and return
+  const command = [cmd];
+  // Strip globaltOpts
+  if (process.lando === 'node') command.push(exports.getOpts());
+  // Add passOpts
+  command.push(passOpts);
+  return _.flatten(command).join(' ');
 };
+
+/*
+ * Helper to get service
+ */
+const getService = (cmd, service) => (_.isObject(cmd)) ? _.first(_.keys(cmd)) : service;
+
+/*
+ * Helper to build commands
+ */
+exports.buildCommand = (app, command, needs, service, user) => ({
+  id: [app.name, service, '1'].join('_'),
+  compose: app.compose,
+  project: app.name,
+  cmd: command,
+  opts: {
+    app: app,
+    mode: 'attach',
+    pre: ['cd', exports.getContainerPath(app.root)].join(' '),
+    user: user,
+    services: _.compact(_.flatten([service, needs])),
+    hijack: false,
+  },
+});
 
 /*
  * Helper to map the cwd on the host to the one in the container
@@ -27,3 +54,58 @@ exports.getContainerPath = appRoot => {
   // Return the directory
   return dir.join('/');
 };
+
+/*
+ * Helper to process args
+ *
+ * We assume pass through commands so let's use argv directly and strip out
+ * the first three assuming they are [node, lando.js, options.name]'
+ * Check to see if we have global lando opts and remove them if we do
+ */
+exports.getOpts = (argopts = process.argv.slice(3)) => {
+  return (_.indexOf(argopts, '--') >= 0) ? _.slice(argopts, 0, _.indexOf(argopts, '--')) : argopts;
+};
+
+/*
+ * Helper to get passthru options
+ */
+exports.getPassthruOpts = (options = {}, answers = {}) => _(options)
+  .filter(value => value.passthrough = true)
+  .map(value => `--${value.name} ${answers[value.name]}`)
+  .value();
+
+/*
+ * Helper to parse tooling config options
+ */
+exports.parseCommand = (cmd, service, passOpts = []) => _.map(!_.isArray(cmd) ? [cmd] : cmd, command => ({
+  command: getCommand(command, passOpts),
+  container: getService(command, service),
+}));
+
+/*
+ * Helper to emit events, run commands and catch errors
+ */
+exports.runCommands = (name, events, runner, cmds, inject = {}) => events.emit(['pre', name].join('-'), inject)
+  // Run commands
+  .then(() => runner(cmds))
+  // Catch error but hide the stdout
+  .catch(error => {
+    error.hide = true;
+    throw error;
+  })
+  // Post event
+  .then(() => events.emit(['post', name].join('-'), inject));
+
+/*
+ * Helper to get defaults
+ */
+exports.toolingDefaults = ({
+  name,
+  app = {},
+  cmd = name,
+  description = `Run ${name} commands`,
+  needs = [],
+  options = {},
+  service = '',
+  user = _.get(app, `services[${service}].environment.LANDO_WEBROOT_USER`, 'root')} = {}) =>
+  ({name, app, cmd, description, needs, options, service, user});
--- plugins/lando-tooling/tooling.js
+++ plugins/lando-tooling/tooling.js
@@ -1,167 +1,43 @@
 'use strict';
 
-const format = require('util').format;
+const _ = require('lodash');
 const utils = require('./lib/utils');
 
-module.exports = lando => {
-  // Modules
-  const _ = lando.node._;
-  const Promise = lando.Promise;
-
+module.exports = lando => ({
   /**
    * The tooling command builder
+   * @todo: this definitely needs work!
    *
    * @since 3.0.0
    * @alias lando.tooling.build
    * @param {Object} config The build config
    * @return {Object} The build object
    */
-  const build = config => {
+  build: config => {
+    const {name, app, cmd, description, needs, options, service, user} = utils.toolingDefaults(config);
     // Get the run handler
     const run = answers => {
-      // Handle dynamic services right off the bat
-      if (_.startsWith(_.get(config, 'service'), ':')) {
-        const serviceOption = config.service.split(':')[1];
-        config.service = answers[serviceOption];
-      }
-
-      // Kick off some collectors
-      let ids = [config.service];
-      let existsCheck = [];
-      let runCheck = [];
-
-      // Add our needs
-      if (_.has(config, 'needs')) {
-        // Normalize
-        if (!_.isArray(config.needs)) {
-          config.needs = [config.needs];
-        }
-
-        // And add
-        ids = _.uniq(_.flatten(ids.concat(config.needs)));
-      }
-
-      // Build our checkers
-      _.forEach(ids, id => {
-        const container = [config.app.name, id, '1'].join('_');
-        existsCheck.push(lando.engine.exists({id: container}));
-        runCheck.push(lando.engine.isRunning(container));
+      // Handle dynamic services right away
+      const container = (_.startsWith(service, ':')) ? answers[service.split(':')[1]] : service;
+      // Normalize any needs we have
+      const auxServices = (_.isString(needs)) ? [needs] : needs;
+      // Get passthrough options
+      const passOpts = utils.getPassthruOpts(options, answers);
+      // Translate our command into something we can iterate over
+      const tooling = _.map(utils.parseCommand(cmd, container, passOpts), ({command, container}) => {
+        return utils.buildCommand(app, command, auxServices, container, user);
       });
-
-      // Let's check to see if the container exists
-      return Promise.all(existsCheck)
-
-      // If they dont, immediately start, if not check if they are running already
-      .then(exists => {
-        const descartes = (current, exist) => {
-          return current && exist;
-        };
-
-        // Pass to should start if something doesnt exist
-        if (!_.reduce(exists, descartes, true)) {
-          return true;
-        } else {
-          return Promise.all(runCheck)
-          .then(isRunning => {
-            return !_.reduce(isRunning, descartes, true);
-          });
-        }
-      })
-
-      // If not let's make sure we start it
-      .then(shouldStart => {
-        if (shouldStart) {
-          _.set(config, 'app.opts.services', ids);
-          return lando.engine.start(config.app);
-        }
-      })
-
-      // Run the command
-      .then(() => {
-        // Standardize and arrayify
-        if (_.has(config, 'cmd')) {
-          if (!_.isString(config.cmd) && !_.isObjectLike(config.cmd)) {
-            config.cmd = _.toString(config.cmd);
-          }
-          if (_.has(config, 'cmd') && _.isString(config.cmd)) {
-            config.cmd = config.cmd.split(' ');
-          }
-        }
-
-        // Start with the entrypoint
-        let cmd = config.cmd || [config.name];
-
-        // Add in args if we expect them
-        if (lando.config.process === 'node') {
-          cmd = cmd.concat(utils.stripGlobalOpts());
-        }
-
-        // Get the backup user
-        const userPath = 'environment.LANDO_WEBROOT_USER';
-        const user = _.get(config.app.services[config.service], userPath, 'root');
-        const name = config.name;
-        const eventName = name.split(' ')[0];
-
-        // Build out our options
-        const options = {
-          id: [config.app.name, config.service, '1'].join('_'),
-          compose: config.app.compose,
-          project: config.app.name,
-          cmd: cmd,
-          opts: {
-            app: config.app,
-            mode: 'attach',
-            pre: ['cd', utils.getContainerPath(config.app.root)].join(' '),
-            user: config.user || user,
-            services: [config.service],
-            hijack: config.hijack || false,
-          },
-        };
-
-        // If this is a specal "passthrough" command lets make sure we are
-        // appending options
-        _.forEach(config.options, (option, key) => {
-          if (option.passthrough && key) {
-            cmd.push('--' + key);
-            cmd.push(answers[key]);
-          }
-        });
-
-        // Run a pre-event
-        return config.app.events.emit(['pre', eventName].join('-'), config)
-
-        // Exec
-        .then(() => {
-          return lando.engine.run(options);
-        })
-
-        // Check for error code and kill with that code, this ensures that
-        // the correct error code is bubbling up and should help provide similar
-        // experience when running these commands in something like travis
-        .catch(error => {
-          error.hide = true;
-          throw error;
-        })
-
-        // Post event
-        .then(() => {
-          return config.app.events.emit(['post', eventName].join('-'), config);
-        });
-      });
+      // Run a pre-event
+      return utils.runCommands(name.split(' ')[0], app.events, lando.engine.run, tooling, config);
     };
 
     // Return our tasks
     return {
-      name: _.first(config.name.split(' ')),
-      command: config.name,
-      describe: config.description || format('Run %s commands', config.name),
+      name: _.first(name.split(' ')),
+      command: name,
+      describe: description,
       run: run,
-      options: config.options || {},
+      options: options,
     };
-  };
-
-  // Return things
-  return {
-    build: build,
-  };
-};
+  },
+});
