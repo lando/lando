5b1907272d2d2b142c5202927611e12f62877aef
Dustin LeBlanc <dustin.m.leblanc@gmail.com>
2018-06-25 18:36:48 -0400

    WIP

--- plugins/lando-engine/test/unit/daemon.spec.js
+++ plugins/lando-engine/test/unit/daemon.spec.js
@@ -6,19 +6,45 @@
 'use strict';
 
 // Setup chai.
-// const chai = require('chai');
-// const expect = chai.expect;
-// chai.should();
+const chai = require('chai');
+chai.should();
+// const sinon = require('sinon');
+const Daemon = require('../../lib/daemon');
+const env = require('../../lib/env');
+const Log = require('../../../../lib/logger');
+// const Events = require('../../../../lib/events');
 
 describe('lando-engine.daemon', () => {
   describe('#LandoDaemon', () => {
-    it('should return an instance with the correct defaults');
+    it('should return an instance with the correct defaults', () => {
+      const daemon = new Daemon();
+
+      daemon.docker.should.equal(env.getDockerExecutable());
+      daemon.log.should.be.instanceOf(Log);
+      return daemon.context.should.equal('node');
+    });
+
+    // @todo this seems like a dupe of the above...
     it('should set this with the correct things');
   });
 
   describe('#up', () => {
-    // Using a sinon stub or spy on core EventEmitter is probably fine here
-    it('should emit a pre and post engine-up events');
+    // // Using a sinon stub or spy on core EventEmitter is probably fine here
+    // it('should emit a pre and post engine-up events', () => {
+    //   const spy1 = sinon.spy();
+    //   const spy2 = sinon.spy();
+    //   const events = new Events();
+    //   events.on('pre-engine-up', spy1);
+    //   events.on('pre-engine-down', spy2);
+    //   const daemon = new Daemon(null, events);
+    //   const isUpStub = sinon.stub(daemon, 'isUp').resolves(true);
+    //
+    //   daemon.up();
+    //
+    //   spy1.should.have.been.called;
+    //   spy2.should.have.been.called;
+    //   isUpStub.restore();
+    // });
 
     // @note: probably worth looking at our other tests for platform spoofing goodness
     // @todo: get ^ goodness in some unit test helper module so we dont keep dedoing it on a per-file basis
