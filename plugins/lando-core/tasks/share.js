58ec416b443c229952c1aca14dadb13574bc8639
Mike Pirog <mike@thinktandem.io>
2018-12-03 16:20:05 -0500

    #5000: CC5

--- plugins/lando-core/tasks/share.js
+++ plugins/lando-core/tasks/share.js
@@ -1,12 +1,50 @@
 'use strict';
 
+const _ = require('lodash');
+const chalk = require('chalk');
 const localtunnel = require('localtunnel');
 const path = require('path');
 const u = require('url');
 
+/*
+ * Helper to get localtunnel config
+ */
+const parseConfig = (port = 80, host = 'localhost') => {
+  // Set port to 80 if null
+  if (_.isEmpty(80)) port = 80;
+  // Make sure we are at least 4 characters
+  if (_.size(host) <= 4) host = host + 'xxxx';
+  // Makes sure we are at most 64 chars
+  if (_.size(host) >= 63) host = host.substring(0, 57);
+  return {port, host};
+};
+
+/*
+ * Helper to manage the tunnel
+ */
+const tunnelHandler = tunnel => {
+  // the assigned public url for your tunnel
+  // i.e. https://abcdefgjhij.localtunnel.me
+  console.log(chalk.blue(tunnel.url), '\n');
+  console.log(chalk.yellow('Press any key to close the tunnel.'));
+  // Set stdin to the correct mode
+  // @todo: We will need to change this for better localdev gui usage
+  process.stdin.resume();
+  process.stdin.setEncoding('utf8');
+  process.stdin.setRawMode(true);
+  // Start the keypress listener for the process
+  process.stdin.on('data', () => {
+    tunnel.close();
+  });
+
+  // Close the process
+  tunnel.on('close', () => {
+    console.log(chalk.green('Tunnel closed!'));
+    process.stdin.pause();
+  });
+};
+
 module.exports = lando => {
-  const _ = lando.node._;
-  const chalk = lando.node.chalk;
   return {
     command: 'share',
     describe: 'Shares your local site publicly',
@@ -18,10 +56,7 @@ module.exports = lando => {
       },
     },
     run: options => {
-      // Do some validation of the url
-      const url = u.parse(options.url);
-      // Validate URL
-      if (url.hostname !== 'localhost' || url.protocol !== 'http:') {
+      if (u.parse(options.url).hostname !== 'localhost' || u.parse(options.url).protocol !== 'http:') {
         throw new Error('Need a url of the form http://localhost:port!');
       }
       // Try to get our app
@@ -34,43 +69,15 @@ module.exports = lando => {
         return app.start().then(app => lando.metrics.report('share', {}))
         // Get the URLS
         .then(() => {
-          // Get the port
-          const port = (!_.isEmpty(url.port)) ? url.port : '80';
-          // Translate the app.name into a localtunnel suitable address
-          // eg lowercase/alphanumeric 4-63 chars
-          // lowercase and alphanumeric it
-          let tunnelHost = _.lowerCase(app.name).replace(/[^0-9a-z]/g, '');
-          // Make sure we are at least 4 characters
-          if (_.size(tunnelHost) <= 4) tunnelHost = tunnelHost + 'xxxx';
-          // Makes sure we are at most 64 chars
-          if (_.size(tunnelHost) >= 63) tunnelHost = tunnelHost.substring(0, 57);
+          const config = parseConfig(u.parse(options.url).port, _.lowerCase(app.name).replace(/[^0-9a-z]/g, ''));
           // Set up the localtunnel
-          const tunnel = localtunnel(port, {subdomain: tunnelHost}, (err, tunnel) => {
+          localtunnel(config.port, {subdomain: config.host}, (err, tunnel) => {
             // Error if needed
             if (err) lando.log.error(err);
             // Header it
             console.log(lando.cli.makeArt('tunnel'));
-            // the assigned public url for your tunnel
-            // i.e. https://abcdefgjhij.localtunnel.me
-            console.log(chalk.blue(tunnel.url));
-            console.log('');
-            console.log(chalk.yellow('Press any key to close the tunnel.'));
-            // Set stdin to the correct mode
-            // @todo: We will need to change this for better localdev gui usage
-            process.stdin.resume();
-            process.stdin.setEncoding('utf8');
-            process.stdin.setRawMode(true);
-            // Start the keypress listener for the process
-            process.stdin.on('data', function() {
-              tunnel.close();
-            });
-          });
-
-          // Close the process
-          tunnel.on('close', function() {
-            console.log('');
-            console.log(chalk.green('Tunnel closed!'));
-            process.stdin.pause();
+            // Handler
+            tunnelHandler(tunnel);
           });
         });
       }
