fda099cf9027c19f7c912107f9cc7c8d463de17c
Mike Pirog <mike@thinktandem.io>
2018-12-10 22:12:59 -0500

    #5000: memcached

---
+++ plugins/lando-services/services/memcached/builder.js
@@ -0,0 +1,35 @@
+'use strict';
+
+// Modules
+const _ = require('lodash');
+
+// Builder
+module.exports = {
+  name: 'memcached',
+  config: {
+    version: '1',
+    supported: ['1', '1.5.12'],
+    patchesSupported: true,
+    legacy: [],
+    mem: 64,
+    port: '11211',
+  },
+  parent: '_database',
+  builder: (parent, config) => class LandoMemcached extends parent {
+    constructor(id, options = {}) {
+      options = _.merge({}, config, options);
+      const memcached = {
+        image: `bitnami/memcached:${options.version}`,
+        command: '/app-entrypoint.sh /run.sh',
+        environment: {
+          MEMCACHED_CACHE_SIZE: options.mem,
+        },
+      };
+      // Send it downstream
+      super(id, options, {
+        services: _.set({}, options.name, memcached),
+        volumes: _.set({}, `data_${options.name}`, {}),
+      });
+    };
+  },
+};
