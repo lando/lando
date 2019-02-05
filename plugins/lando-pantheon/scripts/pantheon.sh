1ae7a4669786ad1e5d2095421a2d0f7e56cb8cee
Mike Pirog <mike@thinktandem.io>
2019-02-05 17:55:22 -0500

    #1409: Improve auto wp search-replace for pantehon recipes

--- plugins/lando-pantheon/scripts/pantheon.sh
+++ plugins/lando-pantheon/scripts/pantheon.sh
@@ -7,24 +7,26 @@ echo "Pantheon pre-run scripting"
 
 /helpers/add-cert.sh
 
-# Emulate Pantheon HOME structure
-if [ "$LANDO_SERVICE_NAME" = "appserver" ]; then
+# Set up some new dirs
+mkdir -p /var/www/certs
+mkdir -p /srv/bindings
+chown -R www-data:www-data /var/www/certs /srv/bindings
 
-  # Set up some new dirs
-  mkdir -p /var/www/certs
-  mkdir -p /srv/bindings
-  chown -R www-data:www-data /var/www/certs /srv/bindings
+# Set up some symlnks
+ln -sfn /var/www /srv/bindings/lando
+ln -sfn /tmp /srv/bindings/lando/tmp
+ln -sfn /app /srv/bindings/lando/code
+ln -sfn /certs/cert.pem /var/www/certs/binding.pem
 
-  # Set up some symlnks
-  ln -sfn /var/www /srv/bindings/lando
-  ln -sfn /tmp /srv/bindings/lando/tmp
-  ln -sfn /app /srv/bindings/lando/code
-  ln -sfn /certs/cert.pem /var/www/certs/binding.pem
+# Hook up a custom wp-cli.yml file to help with auto search-replace
+# See: https://github.com/lando/lando/issues/1409
+mkdir -p /var/www/.wp-cli
+cat > /var/www/.wp-cli/config.yml <<EOF
+path: $LANDO_WEBROOT
+EOF
 
-  # Do another chown pass
-  find /var/www -type d -exec chown www-data:www-data {} +
-
-fi
+# Do another chown pass
+find /var/www -type d -exec chown www-data:www-data {} +
 
 # LOCKR integration
 # If we don't have our dev cert already let's get it
