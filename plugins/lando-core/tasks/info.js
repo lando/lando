ce114300906bcb9f749774f9b1858becdd2f8450
Samuel Oltz <soltz@rapiddg.com>
2019-02-12 15:20:18 -0500

    missing commas in info.js

--- plugins/lando-core/tasks/info.js
+++ plugins/lando-core/tasks/info.js
@@ -29,12 +29,12 @@ module.exports = lando => ({
     const app = lando.getApp(options._app.root);
     // Get services
     app.opts = (!_.isEmpty(options.service)) ? {
-      services: options.service
+      services: options.service,
     } : {};
     // Go deep if we need to
     if (app && options.deep) {
       return app.init().then(() => lando.engine.list({
-          app: app.name
+          app: app.name,
         })
         .filter(container => filterServices(container.service, options.service))
         .each(container => lando.engine.scan(container)
