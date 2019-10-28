35c5169c3349b9ec67848ca0903280807dfe77c3
Mike Pirog <mike@thinktandem.io>
2019-10-28 11:46:53 -0400

    Improve bugsnag reporting

--- metrics/plugins/bugsnag.js
+++ metrics/plugins/bugsnag.js
@@ -5,6 +5,36 @@ const bugsnag = require('bugsnag');
 const Promise = require('bluebird');
 
 /*
+ * Return registered data
+ */
+const getRegistrationData = (apiKey, data = {}) => ({
+  apiKey,
+  appType: `${data.context}-${data.mode}`,
+  appVersion: data.version.replace(/-dev$/, ''),
+  releaseStage: data.product || 'kalabox',
+});
+
+/*
+ * Return user data
+ */
+const getUserData = (data = {}) => ({
+  id: data.instance,
+  name: data.name || data.instance,
+  email: data.email || 'unknown',
+});
+
+/*
+ * Return context data
+ */
+const getContextData = (data = {}) => {
+  switch (data.product) {
+    case 'lando': return data.command;
+    case 'pld': return data.operation || data.command;
+    default: return 'kalabox';
+  }
+};
+
+/*
  * Creates a new BS instance.
  */
 class Bugsnag {
@@ -27,18 +57,18 @@ class Bugsnag {
     return Promise.try(() => {
       // Only report errors.
       if (data.action === 'error') {
-        // Register bug snag api from config.
-        bugsnag.register(apiKey, {
-          appVersion: data.version.replace(/-dev$/, ''),
-          releaseStage: data.devMode ? 'development' : 'production',
-        });
+        // Register client and set data for bugsnag.
+        const client = bugsnag(getRegistrationData(apiKey, data));
+        client.user = getUserData(data);
+        client.context = getContextData(data);
+        client.metaData = data;
         // Create a new error with err message.
         const err = new Error(data.message);
         // Add stack trace.
         err.stack = data.stack;
         // Report to bug snag along with full meta data.
         return Promise.fromNode(cb => {
-          bugsnag.notify(err, data, cb);
+          client.notify(err, {}, cb);
         });
       }
     });
