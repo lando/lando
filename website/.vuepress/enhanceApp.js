e82c90ecbb8aebdca04d20d3eab5dfb2dbee1407
Mike Pirog <mike@thinktandem.io>
2019-12-20 17:16:33 -0500

    #0: Only install api plugin in browser

--- website/.vuepress/enhanceApp.js
+++ website/.vuepress/enhanceApp.js
@@ -3,15 +3,17 @@ import axios from 'axios';
  * Use this file to augment vuepress with other vue-y things
  */
 export default ({ Vue, options, router, siteData }) => { // eslint-disable-line
-  Vue.use({
-    install(Vue) {
-      Object.defineProperties(Vue.prototype, {
-        $api: {
-          get() {
-            return (base = 'https://api.lando.dev') => axios.create({baseURL: base});
+  if (typeof process === 'undefined') {
+    Vue.use({
+      install(Vue) {
+        Object.defineProperties(Vue.prototype, {
+          $api: {
+            get() {
+              return (base = 'https://api.lando.dev') => axios.create({baseURL: base});
+            },
           },
-        },
-      });
-    },
-  });
+        });
+      },
+    });
+  }
 };
