fc51090c4d9ff82d2de2e62d2bf7b01053170ecf
Mike Pirog <mike@thinktandem.io>
2020-01-31 19:04:11 -0500

    #0: Add back in a taste of config

--- events/.vuepress/config.js
+++ events/.vuepress/config.js
@@ -1,7 +1,16 @@
+const webpack = require('webpack');
 
 module.exports = {
   title: 'Lando Events and Meetups',
   description: 'A list of liberating events and meetups.',
+  configureWebpack: config => {
+    return {plugins: [
+      new webpack.EnvironmentPlugin({
+        LANDO_API: process.env.LANDO_API || 'https://api.lando.dev',
+        LANDO_GOOGLE_API_KEY: process.env.LANDO_GOOGLE_API_KEY,
+      }),
+    ]};
+  },
   head: [
     ['link', {rel: 'icon', href: '/favicon.ico'}],
     ['link', {rel: 'stylesheet', href: '/styles/overrides.css'}],
@@ -9,5 +18,36 @@ module.exports = {
     ['script', {src: '//js.hs-scripts.com/6478338.js'}],
   ],
   plugins: [
+    ['@vuepress/google-analytics',
+      {
+        ga: 'UA-74237404-5',
+      },
+    ],
+    ['autometa',
+      {
+        site: {
+          name: 'Lando',
+          twitter: 'devwithlando',
+        },
+        canonical_base: 'https://events.lando.dev',
+      },
+    ],
+    ['canonical',
+      {
+        baseURL: 'https://events.lando.dev',
+      },
+    ],
+    ['robots',
+      {
+        host: 'https://events.lando.dev',
+        sitemap: '/sitemap.xml',
+      },
+    ],
+    ['sitemap',
+      {
+        hostname: 'https://events.lando.dev',
+        exclude: ['/404.html'],
+      },
+    ],
   ],
 };
