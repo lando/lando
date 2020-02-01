17287a4f85d00f0734ea6200c152ee6d44d7e0e8
Mike Pirog <mike@thinktandem.io>
2020-01-31 19:00:39 -0500

    #0: Strip out config.js

--- events/.vuepress/config.js
+++ events/.vuepress/config.js
@@ -1,16 +1,7 @@
-const webpack = require('webpack');
 
 module.exports = {
   title: 'Lando Events and Meetups',
   description: 'A list of liberating events and meetups.',
-  configureWebpack: config => {
-    return {plugins: [
-      new webpack.EnvironmentPlugin({
-        LANDO_API: process.env.LANDO_API || 'https://api.lando.dev',
-        LANDO_GOOGLE_API_KEY: process.env.LANDO_GOOGLE_API_KEY,
-      }),
-    ]};
-  },
   head: [
     ['link', {rel: 'icon', href: '/favicon.ico'}],
     ['link', {rel: 'stylesheet', href: '/styles/overrides.css'}],
@@ -18,44 +9,5 @@ module.exports = {
     ['script', {src: '//js.hs-scripts.com/6478338.js'}],
   ],
   plugins: [
-    ['@vuepress/google-analytics',
-      {
-        ga: 'UA-74237404-5',
-      },
-    ],
-    ['autometa',
-      {
-        site: {
-          name: 'Lando',
-          twitter: 'devwithlando',
-        },
-        canonical_base: 'https://events.lando.dev',
-      },
-    ],
-    ['canonical',
-      {
-        baseURL: 'https://events.lando.dev',
-      },
-    ],
-    ['robots',
-      {
-        host: 'https://events.lando.dev',
-        sitemap: '/sitemap.xml',
-      },
-    ],
-    ['sitemap',
-      {
-        hostname: 'https://events.lando.dev',
-        exclude: ['/404.html'],
-      },
-    ],
   ],
-  themeConfig: {
-    editLinks: false,
-    navbar: false,
-    search: false,
-    sidebar: false,
-    nextLinks: false,
-    prevLinks: false,
-  },
 };
