5abd7dee6b5fa7d14d1994436e89058b9fbc5770
Mike Pirog <mike@lando.dev>
2020-04-06 20:42:48 -0400

    #2098: Autofix is too dangerous, lets just warn the user instead

--- plugins/lando-core/tasks/restart.js
+++ plugins/lando-core/tasks/restart.js
@@ -13,12 +13,18 @@ module.exports = lando => {
       // Restart it if we can!
       if (app) {
         console.log(lando.cli.makeArt('appRestart', {name: app.name, phase: 'pre'}));
+        // Normal
         return app.restart().then(() => {
           const status = utils.getStatusChecks(app);
           const type = !_.every(_.values(status)) ? 'report' : 'post';
           console.log(lando.cli.makeArt('appStart', {name: app.name, phase: type}));
           console.log(lando.cli.formatData(utils.startTable(app), {format: 'table'}, {border: false}));
           console.log('');
+        })
+        // Provide help if there is an error
+        .catch(err => {
+          lando.log.debug(err);
+          console.log(lando.cli.makeArt('appStart', {phase: 'error'}));
         });
       }
     },
