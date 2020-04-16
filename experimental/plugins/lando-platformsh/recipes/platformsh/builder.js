5f3587271ce56fae49b628e3e326b620f670463c
Mike Pirog <mike@lando.dev>
2020-04-15 20:07:02 -0400

    #PSH: Start to scaffold out platform services architecture

--- experimental/plugins/lando-platformsh/recipes/platformsh/builder.js
+++ experimental/plugins/lando-platformsh/recipes/platformsh/builder.js
@@ -4,6 +4,38 @@
 const _ = require('lodash');
 
 /*
+  name: 'platformsh',
+  parent: '_lamp',
+  config: {
+    confSrc: __dirname,
+    xdebug: false,
+    webroot: 'web',
+    database: 'mariadb:10.4',
+    services: {
+      appserver: {
+        meUser: 'web',
+        scanner: false,
+        build_as_root_internal: [
+          '/helpers/boot-psh.sh',
+          '/etc/platform/boot',
+        ],
+        overrides: {
+          privileged: true,
+          image: 'docker.registry.platform.sh/php-7.4',
+          command: 'init',
+          environment: {
+            LANDO_WEBROOT_USER: 'web',
+            LANDO_WEBROOT_GROUP: 'web',
+            LANDO_WEBROOT_UID: '10000',
+            LANDO_WEBROOT_GID: '10000',
+            LANDO_NEEDS_EXEC: 'DOEEET',
+          },
+        },
+      },
+    },
+*/
+
+/*
  * Build Lagoon
  */
 module.exports = {
