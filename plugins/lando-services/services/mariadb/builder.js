02a328b8fe273c99209d1f09b088c0e69366b0c5
Mike Pirog <mike@lando.dev>
2020-05-15 01:34:48 -0400

    Guard secondary perm sweeper when host uid and container uid are the same part 2

--- plugins/lando-services/services/mariadb/builder.js
+++ plugins/lando-services/services/mariadb/builder.js
@@ -38,7 +38,7 @@ module.exports = {
       // Ensure the non-root backup perm sweep runs
       // NOTE: we guard against cases where the UID is the same as the bitnami non-root user
       // because this messes things up on circle ci and presumably elsewhere and _should_ be unncessary
-      if (_.get(options, '_app._config.uid', 1000) !== 1001) options._app.nonRoot.push(options.name);
+      if (_.get(options, '_app._config.uid', '1000') !== '1001') options._app.nonRoot.push(options.name);
 
       const mariadb = {
         image: `bitnami/mariadb:${options.version}`,
