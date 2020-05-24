b0c150ff3b05048b5fa9754ef85ba56ff2ae2d6b
Atilla Manyon <smutlord@datyang.com>
2020-05-24 19:14:10 -0400

    #PSH: Add php app tooling

--- experimental/plugins/lando-platformsh/lib/tooling.js
+++ experimental/plugins/lando-platformsh/lib/tooling.js
@@ -1,31 +1,24 @@
 'use strict';
 
 // Modules
-// const _ = require('lodash');
+const _ = require('lodash');
+
+/*
+ * Helper to get php related tooling commands
+ */
+const getPhpTooling = (service = 'app') => ({composer: {service}, php: {service}});
 
 /*
  * Helper to map lagoon type data to a lando service
  */
-/*
-const getToolingByType = type => {
-  switch (type) {
-    case 'php': return 'platformsh-php';
-    case 'mariadb': return 'platformsh-mariadb';
-    case 'mysql': return 'platformsh-mariadb';
-    default: return false;
+const getToolingByType = app => {
+  switch (app.platformsh.type) {
+    case 'php': return getPhpTooling(app.name);
+    default: return {};
   };
 };
-*/
 
 /*
  * Maps parsed platform config into related Lando things
  */
-exports.getAppTooling = app => {
-  // Start with the things we need regardless of type
-  const tooling = {platform: {service: app.name}};
-
-  // Get type specific things
-
-  // Return it al
-  return tooling;
-};
+exports.getAppTooling = app => _.merge({}, {platform: {service: app.name}}, getToolingByType(app));
