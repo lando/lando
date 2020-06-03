818161e88274e69f710f3dc2516485556eaace83
Mike Pirog <mike@lando.dev>
2020-06-03 14:22:00 -0400

    #2309: Allow -m to also be able to specify a target location

--- experimental/plugins/lando-platformsh/scripts/psh-pull.sh
+++ experimental/plugins/lando-platformsh/scripts/psh-pull.sh
@@ -87,10 +87,6 @@ if [ ${#PLATFORM_PULL_RELATIONSHIPS[@]} -eq 0 ]; then
 # Otherwise loop through our relationships and import them
 else
   for PLATFORM_RELATIONSHIP in "${PLATFORM_PULL_RELATIONSHIPS[@]}"; do
-    # TODO:
-    # verify relationship
-    # print useful message
-    # we need a database target
     lando_pink "Importing data from the $PLATFORM_RELATIONSHIP relationship..."
     platform db:dump -r $PLATFORM_RELATIONSHIP -o | $LANDO_CONNECT_DATABASE
   done
@@ -104,12 +100,17 @@ if [ ${#PLATFORM_PULL_MOUNTS[@]} -eq 0 ]; then
 # Otherwise loop through our mounts and download them them
 else
   for PLATFORM_MOUNT in "${PLATFORM_PULL_MOUNTS[@]}"; do
-    # TODO:
-    # verify mount
-    # print useful message
-    # we need a database target
-    lando_pink "Downloading files from the $PLATFORM_MOUNT relationship..."
-    platform mount:download --mount $PLATFORM_MOUNT --target "/app/$PLATFORM_MOUNT" -y
+    # Try to split PLATFORM_MOUNT
+    IFS=':' read -r -a PLATFORM_MOUNT_PARTS <<< "$PLATFORM_MOUNT"
+    # Set the source and target
+    PLATFORM_MOUNT_SOURCE="${PLATFORM_MOUNT_PARTS[0]}"
+    PLATFORM_MOUNT_TARGET="${PLATFORM_MOUNT_PARTS[1]}"
+    # If PLATFORM_MOUNT_TARGET is still empty lets set it from the source
+    if [ -z "$PLATFORM_MOUNT_TARGET" ]; then
+      PLATFORM_MOUNT_TARGET="/app$PLATFORM_MOUNT_SOURCE"
+    fi
+    lando_pink "Downloading files from the $PLATFORM_MOUNT_SOURCE mount into $PLATFORM_MOUNT_TARGET"
+    platform mount:download --mount $PLATFORM_MOUNT_SOURCE --target "$PLATFORM_MOUNT_TARGET" -y
   done
 fi
 
