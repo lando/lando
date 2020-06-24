7c160b9bf1b0699ba374bd6dc75092787b3b9046
Atilla Manyon <smutlord@datyang.com>
2020-06-24 13:28:13 -0400

    #2279 #2280: Fix test commands that we were previously working around

--- examples/platformsh-drupal8/remove-keys.sh
+++ examples/platformsh-drupal8/remove-keys.sh
@@ -4,5 +4,5 @@ KEYNAME="$1"
 for KEY in $(platform ssh-key:list --columns id,title --format csv --no-header 2>/dev/null | grep "$KEYNAME" | sort -r); do
   ID=$(echo "$KEY" | cut -d, -f1)
   echo "Trying to remove key $ID"
-  platform ssh-key:delete $ID
+  platform ssh-key:delete $ID || true
 done
