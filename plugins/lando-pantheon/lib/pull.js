12803a0ec6e01fd53710bc81fa14696385c9e241
Mike Pirog <mike@lando.dev>
2020-07-07 15:47:52 -0400

    #2402: DEBUG 4

--- plugins/lando-pantheon/lib/pull.js
+++ plugins/lando-pantheon/lib/pull.js
@@ -74,14 +74,13 @@ const frameworkType = (framework = 'drupal8') => {
 const buildDbPullCommand = ({framework = 'drupal8', drush_version = 8} = {}) => {
   // Wordpress things are easy
   if (frameworkType(framework) === 'pressy') {
-    return 'terminus wp -- db export -';
+    return 'terminus --no-interaction wp -- db export -';
   }
 
   // Drupal requires some drush version logic, which is extremely annoying
   // the key difference here is the --extra/--extra-dump flag
   const coercedDrushVersion = semver.valid(semver.coerce(drush_version));
-  // const drushDump = 'terminus drush -- sql-dump --structure-tables-list="cache,cache_*"';
-  const drushDump = 'terminus drush -- sql-dump';
+  const drushDump = 'terminus --no-interaction drush -- sql-dump --structure-tables-list="cache,cache_*"';
   if (!_.isNull(coercedDrushVersion) && semver.gte(coercedDrushVersion, '9.0.0')) {
     return `${drushDump} --extra-dump=--column-statistics=0`;
   } else {
