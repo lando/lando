3088b7dfa4318b4a3cf74cbbd3bc740bb4a1f4c2
Mike Pirog <mike@lando.dev>
2020-08-09 10:36:48 -0400

    #2403: Reorganize token loading mechanism to better facilitate token runtime token reset part 2

--- integrations/lando-pantheon/index.js
+++ integrations/lando-pantheon/index.js
@@ -8,34 +8,36 @@ module.exports = lando => {
   lando.log.alsoSanitize('pantheon-auth');
 
   // Attempt to remove a revoked or bad app token so the user is prompted
-  // @TODO: @dustin was thinking that we should print a message of some kind here
-  // to explain the situation
   _.forEach(['pull', 'push', 'switch'], command => {
     lando.events.on(`cli-${command}-answers`, data => {
       if (_.get(data, 'options._app.recipe') === 'pantheon') {
         // Gather the things we need
         const cacheKey = _.get(data, 'options._app.metaCache');
         const meta = lando.cache.get(cacheKey);
-        const cachedToken = _.get(meta, 'token', '');
+        const cachedToken = _.get(meta, 'token', null);
 
-        // Make sure our token is good beforehand
-        return new PantheonApi(cachedToken, lando.log).auth().catch(error => {
-          if (error.message.includes('failed with code 401')) {
-            // Pull the bad token out of the metadata so it isn't used and prompts user for new token.
-            lando.cache.set(cacheKey, _.omit(meta, ['token']), {persist: true});
+        if (!_.isEmpty(cachedToken)) {
+          // Make sure our token is good beforehand
+          return new PantheonApi(cachedToken, lando.log).auth().catch(error => {
+            if (error.message.includes('failed with code 401')) {
+              // Pull the bad token out of the metadata so it isn't used and prompts user for new token.
+              lando.cache.set(cacheKey, _.omit(meta, ['token']), {persist: true});
 
-            // Remove the bad token from the global token cache.
-            const globalTokens = lando.cache.get('pantheon.tokens');
-            const filteredTokens = _(globalTokens)
-              .filter(token => token.token !== cachedToken)
-              .value();
+              // Remove the bad token from the global token cache.
+              const globalTokens = lando.cache.get('pantheon.tokens');
+              const filteredTokens = _(globalTokens)
+                .filter(token => token.token !== cachedToken)
+                .value();
 
-            // Set things correctly for downstream
-            lando.cache.set('pantheon.tokens', filteredTokens, {persist: true});
-            lando.cache.remove(_.get(data, 'options._app.toolingCache'));
-            lando.cli.clearTaskCaches();
-          }
-        });
+              // Set or things correctly for downstream
+              lando.cache.set('pantheon.tokens', filteredTokens, {persist: true});
+              lando.cache.remove(_.get(data, 'options._app.toolingCache'));
+              lando.cli.clearTaskCaches();
+              // This is the money option
+              delete data.options.auth;
+            }
+          });
+        }
       }
     });
   });
