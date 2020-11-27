eb669d947c64ce22eb1087ebf61a859354e87050
Sascha Grossenbacher <saschagros@gmail.com>
2020-11-27 17:30:28 +0100

    Use new setting instead of deprecated config for temp path

--- integrations/lando-platformsh/lib/overrides.js
+++ integrations/lando-platformsh/lib/overrides.js
@@ -3,7 +3,7 @@
 const overrides = {};
 
 // DRUPAL 8
-overrides['d8config:system.file:path:temporary'] = '/tmp';
+overrides['d8settings:file_temp_path'] = '/tmp';
 overrides['d8settings:skip_permissions_hardening'] = 1;
 
 module.exports = overrides;
