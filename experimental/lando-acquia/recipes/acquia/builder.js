5396558bf3f423fa1dfc15bbbc9723098d3caa19
Mike Pirog <mike@lando.dev>
2021-03-17 15:18:10 -0400

    Make sure we dont need a key to pull acli down from source

--- experimental/lando-acquia/recipes/acquia/builder.js
+++ experimental/lando-acquia/recipes/acquia/builder.js
@@ -62,9 +62,11 @@ module.exports = {
       } else {
         // Build from source
         const gitHubUrl = 'https://github.com/acquia/cli.git';
+        // We need this to remove the key requirement
+        const sshOpts = `GIT_SSH_COMMAND='ssh -o PreferredAuthentications=password -o PubkeyAuthentication=no'`;
         options.services.appserver.build.push(...[
           'rm -rf /usr/local/cli',
-          `cd /usr/local/ && git clone ${gitHubUrl} -b "${acliVersion}" && cd cli && composer install`,
+          `cd /usr/local/ && ${sshOpts} git clone ${gitHubUrl} -b "${acliVersion}" && cd cli && composer install`,
           'ln -sf /usr/local/cli/bin/acli /usr/local/bin/acli',
         ]);
       }
