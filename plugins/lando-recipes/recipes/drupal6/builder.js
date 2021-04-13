424754b4bf36428c1a13ed912526bb49bdeb2bf3
Mike Pirog <mike@lando.dev>
2021-04-12 21:01:38 -0400

    Drupal 6 modifications for drush things and tests

--- plugins/lando-recipes/recipes/drupal6/builder.js
+++ plugins/lando-recipes/recipes/drupal6/builder.js
@@ -13,6 +13,9 @@ module.exports = {
     confSrc: __dirname,
     defaultFiles: {},
     php: '5.6',
+    // @NOTE: below seems to be the last known drush version that let you do
+    // drush si -y succesfully
+    drush: '8.4.5',
   },
   builder: (parent, config) => class LandoDrupal6 extends parent {
     constructor(id, options = {}) {
