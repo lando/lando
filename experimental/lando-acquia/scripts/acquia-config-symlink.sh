44ae1ec912c039abab1331b71dd1d7db7f0e5719
Mike Pirog <mike@thinktandem.io>
2021-05-05 21:35:03 -0400

    #2989: purge cli

--- experimental/lando-acquia/scripts/acquia-config-symlink.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Creates a .acquia directory for the project
-# which can be used by the container via symlink
-ACQUIA_CONFIG_DIR="/lando/config/${LANDO_APP_NAME}/.acquia"
-mkdir -p $ACQUIA_CONFIG_DIR
-ln -sfn ${ACQUIA_CONFIG_DIR} /var/www/.acquia
--- plugins/lando-recipes/recipes/lamp/builder.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-// Modules
-const utils = require('./../../lib/utils');
-
-/*
- * Build LAMP
- */
-module.exports = utils.getLampDefaults();
