3b795c14263ad105204786656780a8f97bdde0fe
Mike Pirog <mike@thinktandem.io>
2021-10-28 10:48:23 -0400

    Basic functions test part 2

--- functions/status.js
+++ functions/status.js
@@ -1,6 +1,8 @@
 exports.handler = async (event) => {
+  const pathParts = event.path.split('/');
+  const status = (pathParts[pathParts.length - 1] === 'status') ? 'ok' : pathParts[pathParts.length - 1];
   return {
     statusCode: 200,
-    body: JSON.stringify(event),
+    body: JSON.stringify({status}),
   };
-}
+};
