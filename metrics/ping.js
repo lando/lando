d71b60c0fa5649f882c27002de43971ecdf81286
Mike Pirog <mike@thinktandem.io>
2021-10-28 21:16:56 -0400

    #2989: Netlify redirects

--- metrics/ping.js
+++
@@ -1,4 +0,0 @@
-exports.handler = async () => ({
-  statusCode: 200,
-  body: JSON.stringify({ping: 'pong'}),
-});
--- metrics/status.js
+++
@@ -1,8 +0,0 @@
-exports.handler = async event => {
-  const pathParts = event.path.split('/');
-  const status = (pathParts[pathParts.length - 1] === 'status') ? 'ok' : pathParts[pathParts.length - 1];
-  return {
-    statusCode: 200,
-    body: JSON.stringify({status}),
-  };
-};
